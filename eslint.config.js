77bda3a46e1e37108db48097b45fa80eea6a3909
Kenji Saito <ken-yo@mbr.nifty.com>
2024-04-11 00:23:21 +0900

    Replace to typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,39 @@
+// @ts-check
+
+import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import stylisticTs from '@stylistic/eslint-plugin-ts';
-import parserTs from '@typescript-eslint/parser';
 import stylisticJsx from '@stylistic/eslint-plugin-jsx';
+import tseslint from 'typescript-eslint';
 
-export default [
-  stylistic.configs.customize({
-    // the following options are the default values
-    indent: 2,
-    quotes: 'single',
-    semi: true,
-    jsx: true,
-  }),
-  // your own rules
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
+    ignores: [
+      '**/*.d.ts',
+      '*.{js,jsx}',
+      'src/tsconfig.json',
+      'src/stories',
+      '**/*.css',
+      'node_modules/**/*',
+      './.next/*',
+      'out',
+      '.storybook',
+      'dist',
+    ],
+  },
+  {
+    files: ['src/*.{ts,tsx}', 'src/**/*.{ts,tsx}'],
     plugins: {
       '@stylistic': stylistic,
       '@stylistic/ts': stylisticTs,
       '@stylistic/jsx': stylisticJsx,
     },
-    languageOptions: { parser: parserTs },
     rules: {
       '@stylistic/semi': 'error',
       '@stylistic/ts/indent': ['error', 2],
       '@stylistic/jsx/jsx-indent': ['error', 2],
     },
   },
-  {
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-  },
-];
+);
