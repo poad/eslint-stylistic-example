afcbdba761c10dbc972e01cd38b3c32fb2791543
Kenji Saito <ken-yo@mbr.nifty.com>
2025-06-25 12:51:30 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@ if [ $result -ne 0 ]; then
   exit $result
 fi
 pwd
-npx pnpm@latest self-update && rm -rf node_modules && pnpm up -r && rm -rf node_modules pnpm-lock.yaml && bun i && bun run lint-fix && bun run build
+npx -y pnpm@latest self-update && rm -rf node_modules && pnpm up -r && rm -rf node_modules pnpm-lock.yaml && bun i && bun run lint-fix && bun run build
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
