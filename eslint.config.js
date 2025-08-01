23926144af3d3274b05d5747a0d1da7c81cc5b73
Kenji Saito <ken-yo@mbr.nifty.com>
2025-08-01 20:02:10 +0900

    Bumps node modules

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,20 @@ import pluginPromise from 'eslint-plugin-promise'
 
 import solid from 'eslint-plugin-solid';
 
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat();
+
+import { includeIgnoreFile } from '@eslint/compat';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
 export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
   {
     ignores: [
       '**/*.d.ts',
